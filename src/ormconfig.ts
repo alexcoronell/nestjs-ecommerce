c8c40f83fa9be9883e548d3d800b3447d263e76a
Alex Coronell <alexcoronell@gmail.com>
2025-05-02 17:48:43 -0500

    env file fixed

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import { DataSource } from 'typeorm';
 
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'dev';
 
 config({
   path: `.env.${env}`,

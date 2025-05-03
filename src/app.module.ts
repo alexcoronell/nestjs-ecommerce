8ff230e83acf5a693e106cbeddfb62818ac1ad3f
Alex Coronell <alexcoronell@gmail.com>
2025-05-03 15:14:18 -0500

    Database module completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+
+/* Modules */
+import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
+
+/* Config */
 import config from './config';
 
 @Module({
@@ -13,6 +18,7 @@ import config from './config';
       isGlobal: true,
     }),
     UserModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],

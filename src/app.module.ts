ca4988e8481344a29d9542ede96688bb71163b9d
Alex Coronell <alexcoronell@gmail.com>
2025-05-03 14:45:02 -0500

    userResource removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import config from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV}`,
+      load: [config],
+      isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-      }),
-      inject: [ConfigService],
-    }),
-    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('config', () => {
+  return {
+    postgres: {
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT as string, 10) || 5432,
+      dbName: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      user: process.env.DB_USERNAME,
+    },
+  };
+});

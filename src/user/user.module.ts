6fe2d5fbb7546e992bf5782cdd1d33f707f49565
Alex Coronell <alexcoronell@gmail.com>
2025-05-03 15:16:47 -0500

    User entity added on user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '@user/entities/user.entity';
+import { UserService } from '@user/user.service';
+import { UserController } from '@user/user.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
 })

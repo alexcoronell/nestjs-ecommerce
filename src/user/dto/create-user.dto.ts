365e022c0fad185f848d4460f710f520fa865849
Alex Coronell <alexcoronell@gmail.com>
2025-05-03 16:26:13 -0500

    user dtos completed

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,37 @@
-export class CreateUserDto {}
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly firstname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly lastname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly password: string;
+
+  @IsString()
+  @ApiProperty()
+  readonly address: string;
+
+  @IsString()
+  @ApiProperty()
+  readonly neighborhood: string;
+
+  @IsString()
+  @ApiProperty()
+  readonly phoneNumber: string;
+}

77097bf770097dfc56ad7dcf74198ba800cbdc29
Alex Coronell <alexcoronell@gmail.com>
2025-05-10 12:39:33 -0500

    User service tests -  count services completed

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,66 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConflictException, NotFoundException } from '@nestjs/common';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+/* Services */
 import { UserService } from './user.service';
+
+/* Entity */
+import { User } from '@user/entities/user.entity';
+
+/* DTO's */
+import { CreateUserDto } from '@user/dto/create-user.dto';
+import { UpdateUserDto } from '@user/dto/update-user.dto';
+import { UpdatePasswordDto } from '@user/dto/update-password-user';
+
+/* Faker */
+import { generateUser } from '@faker/user.faker';
 
 describe('UserService', () => {
   let service: UserService;
+  let repository: Repository<User>;
 
-  beforeEach(async () => {
+   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    repository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+   describe('count user services', () => {
+    it('should return total all users', async () => {
+      jest.spyOn(repository, 'count').mockResolvedValue(100);
+
+      const { statusCode, total } = await service.countAll();
+      expect(repository.count).toHaveBeenCalledTimes(1);
+      expect(statusCode).toBe(200);
+      expect(total).toEqual(100);
+    });
+
+    it('should return total users not removed', async () => {
+      jest.spyOn(repository, 'count').mockResolvedValue(100);
+      const { statusCode, total } = await service.count();
+      expect(repository.count).toHaveBeenCalledTimes(1);
+      expect(repository.count).toHaveBeenCalledWith({
+        where: { isDeleted: false },
+      });
+      expect(statusCode).toBe(200);
+      expect(total).toEqual(100);
+    });
   });
 });

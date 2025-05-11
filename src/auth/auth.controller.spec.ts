927c8631cf51f7aff5ab3d2a6c9045a8c983b7a3
Alex Coronell <alexcoronell@gmail.com>
2025-05-10 21:13:34 -0500

    Auth Controller Tests -  Basic implementation

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,20 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+/* Controllers */
 import { AuthController } from './auth.controller';
+
+/* Services */
 import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService;
 
-  beforeEach(async () => {
+   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            login: jest.fn(),
+            validateToken: jest.fn(),
+            refreshToken: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 });

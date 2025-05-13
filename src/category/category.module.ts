7289b3ded0cfa2b328dc7200b1db347037ceac02
Alex Coronell <alexcoronell@gmail.com>
2025-05-12 20:46:11 -0500

    Category entity registered in module

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CategoryService } from './category.service';
-import { CategoryController } from './category.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryService } from '@category/category.service';
+import { CategoryController } from '@category/category.controller';
+import { Category } from '@category/entities/category.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoryController],
   providers: [CategoryService],
 })

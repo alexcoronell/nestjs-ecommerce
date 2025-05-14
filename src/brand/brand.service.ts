320daa1445a4c78228c238d30e6c07103736c1b1
Alex Coronell <alexcoronell@gmail.com>
2025-05-13 19:37:38 -0500

    Brand Service completed

--- src/brand/brand.service.ts
+++ src/brand/brand.service.ts
@@ -1,26 +1,118 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+/* Interfaces */
+import { IBaseService } from '@commons/interfaces/i-base-service';
+
+/* Entities */
+import { Brand } from './entities/brand.entity';
+
+/* DTO's */
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
 
+/* Types */
+import { Result } from '@commons/types/result.type';
+
 @Injectable()
-export class BrandService {
-  create(createBrandDto: CreateBrandDto) {
-    return 'This action adds a new brand';
+export class BrandService
+  implements IBaseService<Brand, CreateBrandDto, UpdateBrandDto>
+{
+  constructor(
+    @InjectRepository(Brand)
+    private readonly repo: Repository<Brand>,
+  ) {}
+
+  async countAll() {
+    const total = await this.repo.count();
+    return { statusCode: HttpStatus.OK, total };
   }
 
-  findAll() {
-    return `This action returns all brand`;
+  async count() {
+    const total = await this.repo.count({
+      where: {
+        isDeleted: false,
+      },
+    });
+    return { statusCode: HttpStatus.OK, total };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} brand`;
+  async findAll() {
+    const [brands, total] = await this.repo.findAndCount({
+      where: {
+        isDeleted: false,
+      },
+      order: {
+        name: 'ASC',
+      },
+    });
+
+    return {
+      statusCode: HttpStatus.OK,
+      data: brands,
+      total,
+    };
   }
 
-  update(id: number, updateBrandDto: UpdateBrandDto) {
-    return `This action updates a #${id} brand`;
+  async findOne(id: Brand['id']): Promise<Result<Brand>> {
+    const brand = await this.repo.findOne({
+      relations: ['createdBy', 'updatedBy'],
+      where: { id, isDeleted: false },
+    });
+    if (!brand) {
+      throw new NotFoundException(`The Brand with id: ${id} not found`);
+    }
+    return {
+      statusCode: HttpStatus.OK,
+      data: brand,
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} brand`;
+  async findOneByName(name: string): Promise<Result<Brand>> {
+    const brand = await this.repo.findOne({
+      relations: ['createdBy', 'updatedBy'],
+      where: { name, isDeleted: false },
+    });
+    if (!brand) {
+      throw new NotFoundException(`The Brand with name: ${name} not found`);
+    }
+    return {
+      statusCode: HttpStatus.OK,
+      data: brand,
+    };
+  }
+
+  async create(dto: CreateBrandDto) {
+    const newBrand = this.repo.create(dto);
+    const brand = await this.repo.save(newBrand);
+    return {
+      statusCode: HttpStatus.CREATED,
+      data: brand,
+      message: 'The Brand was created',
+    };
+  }
+
+  async update(id: number, changes: UpdateBrandDto) {
+    const { data } = await this.findOne(id);
+    this.repo.merge(data as Brand, changes);
+    const rta = await this.repo.save(data as Brand);
+    return {
+      statusCode: HttpStatus.OK,
+      data: rta,
+      message: `The Brand with id: ${id} has been modified`,
+    };
+  }
+
+  async remove(id: Brand['id']) {
+    const { data } = await this.findOne(id);
+
+    const changes = { isDeleted: true };
+    this.repo.merge(data as Brand, changes);
+    await this.repo.save(data as Brand);
+    return {
+      statusCode: HttpStatus.OK,
+      message: `The Brand with id: ${id} has been deleted`,
+    };
   }
 }

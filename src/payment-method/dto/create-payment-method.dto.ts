17c7464ff00da5ef6c6091ed60bb2638d9f13979
Alex Coronell <alexcoronell@gmail.com>
2025-05-14 19:07:43 -0500

    Payment method dto's completed

--- src/payment-method/dto/create-payment-method.dto.ts
+++ src/payment-method/dto/create-payment-method.dto.ts
@@ -1,1 +1,24 @@
-export class CreatePaymentMethodDto {}
+import { IsString, IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreatePaymentMethodDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly createdBy: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly updatedBy: number;
+
+  @IsNumber()
+  @IsOptional()
+  @ApiProperty()
+  readonly deletedBy: number | null;
+}
--- src/payment-method/dto/update-payment-method.dto.ts
+++ src/payment-method/dto/update-payment-method.dto.ts
@@ -1,4 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { PartialType } from '@nestjs/swagger';
+import { OmitType } from '@nestjs/swagger';
 import { CreatePaymentMethodDto } from './create-payment-method.dto';
 
-export class UpdatePaymentMethodDto extends PartialType(CreatePaymentMethodDto) {}
+export class UpdatePaymentMethodDto extends PartialType(
+  OmitType(CreatePaymentMethodDto, ['createdBy']),
+) {}

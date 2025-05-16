ea3f929f564dcb95ad74bd86a0cb986cc6889bd3
Alex Coronell <alexcoronell@gmail.com>
2025-05-16 11:57:10 -0500

    subcategories migration completed

---
+++ src/database/migrations/1747414423347-migrations.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+/* Create Subcategories Table */
+export class Migrations1747414423347 implements MigrationInterface {
+  name = 'Migrations1747414423347';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "subcategories" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, "is_deleted" boolean NOT NULL DEFAULT false, "name" character varying(255) NOT NULL, "category_id" integer, "created_by_user_id" integer, "updated_by_user_id" integer, "deleted_by_user_id" integer, CONSTRAINT "UQ_d1a3a67c9c5d440edf414af1271" UNIQUE ("name"), CONSTRAINT "PK_793ef34ad0a3f86f09d4837007c" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "subcategories" ADD CONSTRAINT "FK_f7b015bc580ae5179ba5a4f42ec" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "subcategories" ADD CONSTRAINT "FK_8d6a231bb3ca1c4fdcc8fd576bb" FOREIGN KEY ("created_by_user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "subcategories" ADD CONSTRAINT "FK_5ce4208f434c6232bb8a5623c6b" FOREIGN KEY ("updated_by_user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "subcategories" ADD CONSTRAINT "FK_3d9d2d64a6c59f88b072894b978" FOREIGN KEY ("deleted_by_user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "subcategories" DROP CONSTRAINT "FK_3d9d2d64a6c59f88b072894b978"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "subcategories" DROP CONSTRAINT "FK_5ce4208f434c6232bb8a5623c6b"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "subcategories" DROP CONSTRAINT "FK_8d6a231bb3ca1c4fdcc8fd576bb"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "subcategories" DROP CONSTRAINT "FK_f7b015bc580ae5179ba5a4f42ec"`,
+    );
+    await queryRunner.query(`DROP TABLE "subcategories"`);
+  }
+}

2a936acc51758eb5b73dfeac68c7f2eaa48a1e6b
Alex Coronell <alexcoronell@gmail.com>
2025-05-16 12:00:30 -0500

    subcategories dto's completed

--- src/subcategories/dto/create-subcategory.dto.ts
+++ src/subcategories/dto/create-subcategory.dto.ts
@@ -1,1 +1,36 @@
-export class CreateSubcategoryDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  Min,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateSubcategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly name: string;
+
+  @IsNumber()
+  @Min(1)
+  @IsNotEmpty()
+  @ApiProperty()
+  category: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly createdBy: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly updatedBy: number;
+
+  @IsNumber()
+  @IsOptional()
+  @ApiProperty()
+  readonly deletedBy: number | null;
+}
--- src/subcategories/dto/update-subcategory.dto.ts
+++ src/subcategories/dto/update-subcategory.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from '@nestjs/swagger';
+import { OmitType } from '@nestjs/swagger';
 import { CreateSubcategoryDto } from './create-subcategory.dto';
 
-export class UpdateSubcategoryDto extends PartialType(CreateSubcategoryDto) {}
+export class UpdateSubcategoryDto extends PartialType(
+  OmitType(CreateSubcategoryDto, ['createdBy']),
+) {}

73dd9a3d4236e7666343b08f4217f9e37bb89b82
Alex Coronell <alexcoronell@gmail.com>
2025-05-16 19:31:53 -0500

    Product dto's completed

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,67 @@
-export class CreateProductDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  Min,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Trim } from '@commons/decorators/trim.decorator';
+
+export class CreateProductDto {
+  @Trim()
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly name: string;
+
+  @Trim()
+  @IsString()
+  @IsOptional()
+  @ApiProperty()
+  readonly description: string;
+
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @Min(0)
+  @IsOptional()
+  @ApiProperty()
+  readonly price: number;
+
+  @IsNumber({ maxDecimalPlaces: 0 })
+  @IsOptional()
+  @ApiProperty()
+  readonly stock: number;
+
+  @IsNumber()
+  @Min(1)
+  @IsNotEmpty()
+  @ApiProperty()
+  category: number;
+
+  @IsNumber()
+  @Min(1)
+  @IsNotEmpty()
+  @ApiProperty()
+  subcategory: number;
+
+  @IsNumber()
+  @Min(1)
+  @IsNotEmpty()
+  @ApiProperty()
+  brandcategory: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly createdBy: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly updatedBy: number;
+
+  @IsNumber()
+  @IsOptional()
+  @ApiProperty()
+  readonly deletedBy: number | null;
+}

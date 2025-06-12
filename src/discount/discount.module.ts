47b9a77fed5da361ee41866e6ae1886d77ab4b54
Alex Coronell <alexcoronell@gmail.com>
2025-06-12 15:27:09 -0500

    Discount table migration completed, product images module fixed

--- src/discount/discount.module.ts
+++ src/discount/discount.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DiscountService } from './discount.service';
 import { DiscountController } from './discount.controller';
+import { Discount } from './entities/discount.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Discount])],
   controllers: [DiscountController],
   providers: [DiscountService],
 })

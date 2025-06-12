b1e3fa5d4b6619eb1f64ee4045a976c3accde69e
Alex Coronell <alexcoronell@gmail.com>
2025-06-12 18:43:39 -0500

    Discount service completed

--- src/discount/discount.service.ts
+++ src/discount/discount.service.ts
@@ -1,26 +1,118 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+/* Interfaces */
+import { IBaseService } from '@commons/interfaces/i-base-service';
+
+/* Entities */
+import { Discount } from './entities/discount.entity';
+
+/* DTO's */
 import { CreateDiscountDto } from './dto/create-discount.dto';
 import { UpdateDiscountDto } from './dto/update-discount.dto';
 
+/* Types */
+import { Result } from '@commons/types/result.type';
+
 @Injectable()
-export class DiscountService {
-  create(createDiscountDto: CreateDiscountDto) {
-    return 'This action adds a new discount';
+export class DiscountService
+  implements IBaseService<Discount, CreateDiscountDto, UpdateDiscountDto>
+{
+  constructor(
+    @InjectRepository(Discount)
+    private readonly repo: Repository<Discount>,
+  ) {}
+
+  async countAll() {
+    const total = await this.repo.count();
+    return { statusCode: HttpStatus.OK, total };
   }
 
-  findAll() {
-    return `This action returns all discount`;
+  async count() {
+    const total = await this.repo.count({
+      where: {
+        isDeleted: false,
+      },
+    });
+    return { statusCode: HttpStatus.OK, total };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} discount`;
+  async findAll() {
+    const [discount, total] = await this.repo.findAndCount({
+      where: {
+        isDeleted: false,
+      },
+      order: {
+        code: 'ASC',
+      },
+    });
+
+    return {
+      statusCode: HttpStatus.OK,
+      data: discount,
+      total,
+    };
   }
 
-  update(id: number, updateDiscountDto: UpdateDiscountDto) {
-    return `This action updates a #${id} discount`;
+  async findOne(id: Discount['id']): Promise<Result<Discount>> {
+    const discount = await this.repo.findOne({
+      relations: ['createdBy', 'updatedBy'],
+      where: { id, isDeleted: false },
+    });
+    if (!discount) {
+      throw new NotFoundException(`The Discount with id: ${id} not found`);
+    }
+    return {
+      statusCode: HttpStatus.OK,
+      data: discount,
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} discount`;
+  async findOneByCode(code: string): Promise<Result<Discount>> {
+    const discount = await this.repo.findOne({
+      relations: ['createdBy', 'updatedBy'],
+      where: { code, isDeleted: false },
+    });
+    if (!discount) {
+      throw new NotFoundException(`The Discount with code: ${code} not found`);
+    }
+    return {
+      statusCode: HttpStatus.OK,
+      data: discount,
+    };
+  }
+
+  async create(dto: CreateDiscountDto) {
+    const newDiscount = this.repo.create(dto);
+    const discount = await this.repo.save(newDiscount);
+    return {
+      statusCode: HttpStatus.CREATED,
+      data: discount,
+      message: 'The Discount was created',
+    };
+  }
+
+  async update(id: number, changes: UpdateDiscountDto) {
+    const { data } = await this.findOne(id);
+    this.repo.merge(data as Discount, changes);
+    const rta = await this.repo.save(data as Discount);
+    return {
+      statusCode: HttpStatus.OK,
+      data: rta,
+      message: `The Discount with id: ${id} has been modified`,
+    };
+  }
+
+  async remove(id: Discount['id']) {
+    const { data } = await this.findOne(id);
+
+    const changes = { isDeleted: true };
+    this.repo.merge(data as Discount, changes);
+    await this.repo.save(data as Discount);
+    return {
+      statusCode: HttpStatus.OK,
+      message: `The Discount with id: ${id} has been deleted`,
+    };
   }
 }

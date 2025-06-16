ad9560f1f6bd4c9409384bac528e54cd78307226
Alex Coronell <alexcoronell@gmail.com>
2025-06-16 17:03:27 -0500

    ProductImages DTO's completed

--- src/product-images/dto/create-product-image.dto.ts
+++ src/product-images/dto/create-product-image.dto.ts
@@ -1,1 +1,39 @@
-export class CreateProductImageDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  Min,
+  IsBoolean,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Trim } from '@commons/decorators/trim.decorator';
+
+export class CreateProductImageDto {
+  @Trim()
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  filePath: string;
+
+  @Trim()
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  title: string;
+
+  @IsBoolean()
+  @IsOptional()
+  @ApiProperty()
+  isMain: boolean;
+
+  @IsNumber()
+  @Min(0)
+  @ApiProperty()
+  product: number;
+
+  @IsNumber()
+  @Min(0)
+  @ApiProperty()
+  uploadedBy: number;
+}
--- src/product-images/dto/update-product-image.dto.ts
+++ src/product-images/dto/update-product-image.dto.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { PartialType } from '@nestjs/swagger';
+import { OmitType } from '@nestjs/swagger';
 import { CreateProductImageDto } from '@product_images/dto/create-product-image.dto';
 
-export class UpdateProductImageDto extends PartialType(CreateProductImageDto) {}
+export class UpdateProductImageDto extends PartialType(
+  OmitType(CreateProductImageDto, ['uploadedBy']),
+) {}

ba3dec0addce7f919518681bd66c12ea490db28c
Alex Coronell <alexcoronell@gmail.com>
2025-06-16 19:08:37 -0500

    Product Tag create dto completed

--- src/product-tag/dto/create-product-tag.dto.ts
+++ src/product-tag/dto/create-product-tag.dto.ts
@@ -1,1 +1,22 @@
-export class CreateProductTagDto {}
+import { Min, IsNotEmpty, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateProductTagDto {
+  @IsNumber()
+  @Min(0)
+  @IsNotEmpty()
+  @ApiProperty()
+  product: number;
+
+  @IsNumber()
+  @Min(0)
+  @IsNotEmpty()
+  @ApiProperty()
+  tag: number;
+
+  @IsNumber()
+  @Min(0)
+  @IsNotEmpty()
+  @ApiProperty()
+  createdBy: number;
+}

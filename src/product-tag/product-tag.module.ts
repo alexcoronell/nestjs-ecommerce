a07d2365d8bbe56dc2c3f0b63ed75a6fabee9761
Alex Coronell <alexcoronell@gmail.com>
2025-06-16 19:31:26 -0500

    Product Tag module completed

--- src/product-tag/product-tag.module.ts
+++ src/product-tag/product-tag.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductTagService } from './product-tag.service';
 import { ProductTagController } from './product-tag.controller';
+import { ProductTag } from './entities/product-tag.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProductTag])],
   controllers: [ProductTagController],
   providers: [ProductTagService],
 })

6e12d508870fc61f02c3f0e5b0363df47b821532
Alex Coronell <alexcoronell@gmail.com>
2025-06-23 14:52:07 -0500

    ProductTags controller completed

--- src/product-tag/product-tag.controller.ts
+++ src/product-tag/product-tag.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+
+/* Services */
 import { ProductTagService } from './product-tag.service';
+
+/* DTO's */
 import { CreateProductTagDto } from './dto/create-product-tag.dto';
 
 @Controller('product-tag')
 export class ProductTagController {
   constructor(private readonly productTagService: ProductTagService) {}
 
-  @Post()
-  create(@Body() createProductTagDto: CreateProductTagDto) {
-    return this.productTagService.create(createProductTagDto);
+  @Get('count-all')
+  countAll() {
+    return this.productTagService.countAll();
   }
 
   @Get()
@@ -17,12 +29,27 @@ export class ProductTagController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: number) {
     return this.productTagService.findOne(+id);
   }
 
+  @Get('by-product/:id')
+  findAllByProduct(@Param('id', ParseIntPipe) id: number) {
+    return this.productTagService.findAllByProduct(+id);
+  }
+
+  @Get('by-tag/:id')
+  findAllByTag(@Param('id', ParseIntPipe) id: number) {
+    return this.productTagService.findAllByTag(+id);
+  }
+
+  @Post()
+  create(@Body() payload: CreateProductTagDto) {
+    return this.productTagService.create(payload);
+  }
+
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseIntPipe) id: number) {
     return this.productTagService.remove(+id);
   }
 }

ca653076731e6ebbfadc511fd88b8dd082f7c207
Alex Coronell <alexcoronell@gmail.com>
2025-06-26 19:02:26 -0500

    Product Suppliers migration completed

---
+++ src/database/migrations/1750982402048-migrations.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+/* Create Product Suppliers Table */
+export class Migrations1750982402048 implements MigrationInterface {
+  name = 'Migrations1750982402048';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "product_suppliers" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, "is_deleted" boolean NOT NULL DEFAULT false, "supplier_product_code" character varying(256) NOT NULL, "cost_price" numeric(10) NOT NULL DEFAULT '0', "is_primary_supplier" boolean NOT NULL DEFAULT false, "product_id" integer, "created_by_user_id" integer, "updated_by_user_id" integer, "deleted_by_user_id" integer, CONSTRAINT "PK_96f9e4cfe1a097fdd2a9a67257a" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "product_suppliers" ADD CONSTRAINT "FK_c1b61c92463463f577fac49b95c" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "product_suppliers" ADD CONSTRAINT "FK_6764e5ab172245e60e9a4020232" FOREIGN KEY ("created_by_user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "product_suppliers" ADD CONSTRAINT "FK_b90d66a2ce63d6855f5073318f6" FOREIGN KEY ("updated_by_user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "product_suppliers" ADD CONSTRAINT "FK_ce47daf62b0c2e2c9f2763d4ca6" FOREIGN KEY ("deleted_by_user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "product_suppliers" DROP CONSTRAINT "FK_ce47daf62b0c2e2c9f2763d4ca6"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "product_suppliers" DROP CONSTRAINT "FK_b90d66a2ce63d6855f5073318f6"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "product_suppliers" DROP CONSTRAINT "FK_6764e5ab172245e60e9a4020232"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "product_suppliers" DROP CONSTRAINT "FK_c1b61c92463463f577fac49b95c"`,
+    );
+    await queryRunner.query(`DROP TABLE "product_suppliers"`);
+  }
+}
--- src/product-supplier/product-supplier.module.ts
+++ src/product-supplier/product-supplier.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductSupplierService } from './product-supplier.service';
 import { ProductSupplierController } from './product-supplier.controller';
+import { ProductSupplier } from './entities/product-supplier.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProductSupplier])],
   controllers: [ProductSupplierController],
   providers: [ProductSupplierService],
 })

dbeeb33092c1828476988174acbb4fc0465b1ecb
Alex Coronell <alexcoronell@gmail.com>
2025-06-26 19:30:59 -0500

    Product Suppliers dtos completed

--- src/product-supplier/dto/create-product-supplier.dto.ts
+++ src/product-supplier/dto/create-product-supplier.dto.ts
@@ -1,1 +1,50 @@
-export class CreateProductSupplierDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  Min,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Trim } from '@commons/decorators/trim.decorator';
+
+export class CreateProductSupplierDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @Trim()
+  supplierProductCode: string;
+
+  @ApiProperty()
+  @IsNumber()
+  @Min(0)
+  costPrice: number;
+
+  @ApiProperty()
+  @IsOptional()
+  isPrimarySupplier: boolean;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  product: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  supplier: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  createdBy: number;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  updatedBy: number;
+
+  @ApiProperty()
+  @IsNumber()
+  deletedBy: number | null;
+}
--- src/product-supplier/dto/update-product-supplier.dto.ts
+++ src/product-supplier/dto/update-product-supplier.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from '@nestjs/swagger';
+import { OmitType } from '@nestjs/swagger';
 import { CreateProductSupplierDto } from './create-product-supplier.dto';
 
-export class UpdateProductSupplierDto extends PartialType(CreateProductSupplierDto) {}
+export class UpdateProductSupplierDto extends PartialType(
+  OmitType(CreateProductSupplierDto, ['createdBy']),
+) {}

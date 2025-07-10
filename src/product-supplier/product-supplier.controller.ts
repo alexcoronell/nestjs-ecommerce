cfaa5b6ffa5c307891c5892558d52285227f1da2
Alex Coronell <alexcoronell@gmail.com>
2025-07-10 18:19:42 -0500

    Product Suppliers controller completed

--- src/product-supplier/product-supplier.controller.ts
+++ src/product-supplier/product-supplier.controller.ts
@@ -1,15 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+
+/* Interface */
+import { IBaseController } from '@commons/interfaces/i-base-controller';
+
+/* Services */
 import { ProductSupplierService } from './product-supplier.service';
+
+/* Entities */
+import { ProductSupplier } from './entities/product-supplier.entity';
+
+/* DTO's */
 import { CreateProductSupplierDto } from './dto/create-product-supplier.dto';
 import { UpdateProductSupplierDto } from './dto/update-product-supplier.dto';
 
 @Controller('product-supplier')
-export class ProductSupplierController {
-  constructor(private readonly productSupplierService: ProductSupplierService) {}
+export class ProductSupplierController
+  implements
+    IBaseController<
+      ProductSupplier,
+      CreateProductSupplierDto,
+      UpdateProductSupplierDto
+    >
+{
+  constructor(
+    private readonly productSupplierService: ProductSupplierService,
+  ) {}
 
-  @Post()
-  create(@Body() createProductSupplierDto: CreateProductSupplierDto) {
-    return this.productSupplierService.create(createProductSupplierDto);
+  @Get('count-all')
+  countAll() {
+    return this.productSupplierService.countAll();
+  }
+
+  @Get('count')
+  count() {
+    return this.productSupplierService.count();
   }
 
   @Get()
@@ -18,17 +51,35 @@ export class ProductSupplierController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: number) {
     return this.productSupplierService.findOne(+id);
   }
 
+  @Get('by-product/:id')
+  findAllByProduct(@Param('id', ParseIntPipe) id: number) {
+    return this.productSupplierService.findAllByProduct(+id);
+  }
+
+  @Get('by-supplier/:id')
+  findAllBySupplier(@Param('id', ParseIntPipe) id: number) {
+    return this.productSupplierService.findAllBySupplier(+id);
+  }
+
+  @Post()
+  create(@Body() payload: CreateProductSupplierDto) {
+    return this.productSupplierService.create(payload);
+  }
+
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductSupplierDto: UpdateProductSupplierDto) {
-    return this.productSupplierService.update(+id, updateProductSupplierDto);
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateCategoryDto: UpdateProductSupplierDto,
+  ) {
+    return this.productSupplierService.update(+id, updateCategoryDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseIntPipe) id: number) {
     return this.productSupplierService.remove(+id);
   }
 }

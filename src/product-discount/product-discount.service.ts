63ba40aa671451647c980fc36a5975eb74f39dcf
Alex Coronell <alexcoronell@gmail.com>
2025-07-16 18:59:07 -0500

    ProductDiscount service completed

--- src/product-discount/product-discount.service.ts
+++ src/product-discount/product-discount.service.ts
@@ -1,21 +1,120 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+/* Entities */
+import { ProductDiscount } from './entities/product-discount.entity';
+import { Product } from '@product/entities/product.entity';
+import { Discount } from '@discount/entities/discount.entity';
+
+/* DTO's */
 import { CreateProductDiscountDto } from './dto/create-product-discount.dto';
+
+/* Types */
+import { Result } from '@commons/types/result.type';
 
 @Injectable()
 export class ProductDiscountService {
-  create(createProductDiscountDto: CreateProductDiscountDto) {
-    return 'This action adds a new productDiscount';
+  constructor(
+    @InjectRepository(ProductDiscount)
+    private readonly repo: Repository<ProductDiscount>,
+  ) {}
+
+  async countAll() {
+    const total = await this.repo.count();
+    return { statusCode: HttpStatus.OK, total };
   }
 
-  findAll() {
-    return `This action returns all productDiscount`;
+  async findAll(): Promise<Result<ProductDiscount[]>> {
+    const [productDiscounts, total] = await this.repo.findAndCount();
+
+    return {
+      statusCode: HttpStatus.OK,
+      data: productDiscounts,
+      total,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} productDiscount`;
+  async findOne(
+    criteria: Partial<Pick<ProductDiscount, 'productId' | 'discountId'>>,
+  ): Promise<Result<ProductDiscount>> {
+    const productDiscount = await this.repo.findOne({
+      where: criteria,
+    });
+    if (!productDiscount) {
+      throw new NotFoundException(
+        `The Product Discount with criteria: ${JSON.stringify(criteria)} not found`,
+      );
+    }
+    return {
+      statusCode: HttpStatus.OK,
+      data: productDiscount,
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} productDiscount`;
+  async findAllByProduct(
+    id: Product['id'],
+  ): Promise<Result<ProductDiscount[]>> {
+    const [productDiscounts, total] = await this.repo.findAndCount({
+      relations: ['product', 'discount'],
+      where: { product: id },
+    });
+    return {
+      statusCode: HttpStatus.OK,
+      data: productDiscounts,
+      total,
+    };
+  }
+
+  async findAllByDiscount(
+    id: Discount['id'],
+  ): Promise<Result<ProductDiscount[]>> {
+    const [productDiscounts, total] = await this.repo.findAndCount({
+      relations: ['product', 'discount'],
+      where: { discount: id },
+    });
+    return {
+      statusCode: HttpStatus.OK,
+      data: productDiscounts,
+      total,
+    };
+  }
+
+  async create(
+    createProductDiscountDto: CreateProductDiscountDto,
+  ): Promise<Result<ProductDiscount>> {
+    const productDiscount = this.repo.create(createProductDiscountDto);
+    await this.repo.save(productDiscount);
+    return {
+      statusCode: HttpStatus.CREATED,
+      data: productDiscount,
+    };
+  }
+
+  async createMany(
+    dtos: CreateProductDiscountDto | CreateProductDiscountDto[],
+  ): Promise<Result<ProductDiscount[]>> {
+    const dtosArray = Array.isArray(dtos) ? dtos : [dtos];
+    const newProductDiscounts = this.repo.create(dtosArray);
+    const productDiscounts = await this.repo.save(newProductDiscounts);
+    return {
+      statusCode: HttpStatus.CREATED,
+      data: productDiscounts,
+    };
+  }
+
+  async delete(
+    criteria: Partial<Pick<ProductDiscount, 'productId' | 'discountId'>>,
+  ): Promise<Result<void>> {
+    const result = await this.repo.delete(criteria);
+    if (result.affected === 0) {
+      throw new NotFoundException(
+        `The Product Discount with criteria: ${JSON.stringify(criteria)} not found`,
+      );
+    }
+    return {
+      statusCode: HttpStatus.OK,
+      message: `The Product Discount with criteria: ${JSON.stringify(criteria)} deleted successfully`,
+    };
   }
 }

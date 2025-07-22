b05a7f464d8bc9486a8850332b179545323f0d6e
Alex Coronell <alexcoronell@gmail.com>
2025-07-21 19:27:58 -0500

    Wishlist service completed

--- src/wishlist/wishlist.service.ts
+++ src/wishlist/wishlist.service.ts
@@ -1,26 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Wishlist } from './entities/wishlist.entity';
 import { CreateWishlistDto } from './dto/create-wishlist.dto';
-import { UpdateWishlistDto } from './dto/update-wishlist.dto';
+import { User } from '../user/entities/user.entity';
+import { Product } from '../product/entities/product.entity';
 
 @Injectable()
 export class WishlistService {
-  create(createWishlistDto: CreateWishlistDto) {
-    return 'This action adds a new wishlist';
+  constructor(
+    @InjectRepository(Wishlist)
+    private readonly repo: Repository<Wishlist>,
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
+    @InjectRepository(Product)
+    private readonly productRepo: Repository<Product>,
+  ) {}
+
+  async findAll() {
+    const [wishlists, total] = await this.repo.findAndCount({
+      relations: ['user', 'product'],
+      order: { addedDate: 'DESC' },
+    });
+    return {
+      statusCode: HttpStatus.OK,
+      data: wishlists,
+      total,
+    };
   }
 
-  findAll() {
-    return `This action returns all wishlist`;
+  async findOne(id: number) {
+    const wishlist = await this.repo.findOne({
+      where: { id },
+      relations: ['user', 'product'],
+    });
+    if (!wishlist) {
+      throw new NotFoundException(`Wishlist with id: ${id} not found`);
+    }
+    return {
+      statusCode: HttpStatus.OK,
+      data: wishlist,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} wishlist`;
+  async create(dto: CreateWishlistDto) {
+    const user = await this.userRepo.findOne({ where: { id: dto.user } });
+    if (!user)
+      throw new NotFoundException(`User with id: ${dto.user} not found`);
+
+    const product = await this.productRepo.findOne({
+      where: { id: dto.product },
+    });
+    if (!product)
+      throw new NotFoundException(`Product with id: ${dto.product} not found`);
+
+    const wishlist = this.repo.create({
+      user,
+      product,
+    });
+    const saved = await this.repo.save(wishlist);
+    return {
+      statusCode: HttpStatus.CREATED,
+      data: saved,
+      message: 'Wishlist item created',
+    };
   }
 
-  update(id: number, updateWishlistDto: UpdateWishlistDto) {
-    return `This action updates a #${id} wishlist`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} wishlist`;
+  async remove(id: number) {
+    const { data } = await this.findOne(id);
+    await this.repo.remove(data);
+    return {
+      statusCode: HttpStatus.OK,
+      message: `Wishlist with id: ${id} deleted`,
+    };
   }
 }

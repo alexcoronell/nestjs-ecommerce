3875757b3a45b1e36a647c1a8d26c5be048c9bdf
Alex Coronell <alexcoronell@gmail.com>
2025-07-30 22:13:24 -0500

    createSaleDto completed

--- src/sale/dto/create-sale.dto.ts
+++ src/sale/dto/create-sale.dto.ts
@@ -1,1 +1,38 @@
-export class CreateSaleDto {}
+import { IsString, IsNotEmpty, IsNumber, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateSaleDto {
+  @IsNumber()
+  @Min(0)
+  @IsNotEmpty()
+  @ApiProperty()
+  totalAmount: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  shippingAddress: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  shippingStatus: string;
+
+  @IsNumber()
+  @Min(0)
+  @IsNotEmpty()
+  @ApiProperty()
+  user: number;
+
+  @IsNumber()
+  @Min(0)
+  @IsNotEmpty()
+  @ApiProperty()
+  paymentMethod: number;
+
+  @IsNumber()
+  @Min(0)
+  @IsNotEmpty()
+  @ApiProperty()
+  shippingCompany: number;
+}

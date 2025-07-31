d4721b4ee0df9a62aeb30d3d71c58a539a9f1cca
Alex Coronell <alexcoronell@gmail.com>
2025-07-31 10:36:06 -0500

    Sale module tests completed

---
+++ src/sale/sale.module.spec.ts
@@ -0,0 +1,49 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { SaleModule } from './sale.module';
+import { SaleService } from './sale.service';
+import { SaleController } from './sale.controller';
+import { Sale } from './entities/sale.entity';
+
+describe('Category module', () => {
+  let module: TestingModule;
+  let service: SaleService;
+  let controller: SaleController;
+  let repository: Repository<Sale>;
+
+  beforeEach(async () => {
+    module = await Test.createTestingModule({
+      imports: [SaleModule],
+    })
+      .overrideProvider(getRepositoryToken(Sale))
+      .useValue({
+        findOne: jest.fn(),
+        save: jest.fn(),
+        find: jest.fn(),
+        delete: jest.fn(),
+      })
+      .compile();
+
+    service = module.get<SaleService>(SaleService);
+    controller = module.get<SaleController>(SaleController);
+    repository = module.get<Repository<Sale>>(getRepositoryToken(Sale));
+  });
+
+  it('should be defined', () => {
+    expect(module).toBeDefined();
+    expect(service).toBeDefined();
+    expect(controller).toBeDefined();
+    expect(repository).toBeDefined();
+  });
+
+  it('should have SaleService and SaleController', () => {
+    expect(module.get(SaleService)).toBeInstanceOf(SaleService);
+    expect(module.get(SaleController)).toBeInstanceOf(SaleController);
+  });
+
+  it('should inject TypeORM repository for Sale', () => {
+    expect(module.get(getRepositoryToken(Sale))).toBeDefined();
+  });
+});

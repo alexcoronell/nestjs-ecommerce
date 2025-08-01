85a1fd3479fdbdb21cefd722cfa41290380e7e24
Alex Coronell <alexcoronell@gmail.com>
2025-08-01 18:40:57 -0500

    Sale details controller completed

--- src/sale-detail/sale-detail.controller.ts
+++ src/sale-detail/sale-detail.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+
+/* Services */
 import { SaleDetailService } from './sale-detail.service';
+
+/* DTO's */
 import { CreateSaleDetailDto } from './dto/create-sale-detail.dto';
 
 @Controller('sale-detail')
 export class SaleDetailController {
   constructor(private readonly saleDetailService: SaleDetailService) {}
 
-  @Post()
-  create(@Body() dtos: CreateSaleDetailDto[]) {
-    return this.saleDetailService.create(dtos);
+  @Get('count')
+  count() {
+    return this.saleDetailService.count();
   }
 
   @Get()
@@ -17,7 +28,17 @@ export class SaleDetailController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.saleDetailService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.saleDetailService.findOne(id);
+  }
+
+  @Get('sale/:id')
+  findBySaleId(@Param('id', ParseIntPipe) id: number) {
+    return this.saleDetailService.findBySaleId(id);
+  }
+
+  @Post()
+  create(@Body() payload: CreateSaleDetailDto[]) {
+    return this.saleDetailService.create(payload);
   }
 }

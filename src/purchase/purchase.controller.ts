5a711f493514b193686b9833bf1be0bd86f2f5ef
Alex Coronell <alexcoronell@gmail.com>
2025-08-02 12:47:27 -0500

    Purchase controller completed

--- src/purchase/purchase.controller.ts
+++ src/purchase/purchase.controller.ts
@@ -1,15 +1,41 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+
+/* Interface */
+import { IBaseController } from '@commons/interfaces/i-base-controller';
+
+/* Services */
 import { PurchaseService } from './purchase.service';
+
+/* Entities */
+import { Purchase } from './entities/purchase.entity';
+
+/* DTO's */
 import { CreatePurchaseDto } from './dto/create-purchase.dto';
 import { UpdatePurchaseDto } from './dto/update-purchase.dto';
 
 @Controller('purchase')
-export class PurchaseController {
+export class PurchaseController
+  implements IBaseController<Purchase, CreatePurchaseDto, UpdatePurchaseDto>
+{
   constructor(private readonly purchaseService: PurchaseService) {}
 
-  @Post()
-  create(@Body() createPurchaseDto: CreatePurchaseDto) {
-    return this.purchaseService.create(createPurchaseDto);
+  @Get('count-all')
+  countAll() {
+    return this.purchaseService.countAll();
+  }
+
+  @Get('count')
+  count() {
+    return this.purchaseService.count();
   }
 
   @Get()
@@ -18,17 +44,30 @@ export class PurchaseController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: number) {
     return this.purchaseService.findOne(+id);
   }
 
+  @Get('supplier/:id')
+  findOneByUserId(@Param('id', ParseIntPipe) id: number) {
+    return this.purchaseService.findBySupplierId(id);
+  }
+
+  @Post()
+  create(@Body() dto: CreatePurchaseDto) {
+    return this.purchaseService.create(dto);
+  }
+
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePurchaseDto: UpdatePurchaseDto) {
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updatePurchaseDto: UpdatePurchaseDto,
+  ) {
     return this.purchaseService.update(+id, updatePurchaseDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseIntPipe) id: number) {
     return this.purchaseService.remove(+id);
   }
 }

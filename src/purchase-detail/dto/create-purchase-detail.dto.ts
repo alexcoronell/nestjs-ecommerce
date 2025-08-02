603fb93c9cc76a8b8a71047e464f5f4bda911074
Alex Coronell <alexcoronell@gmail.com>
2025-08-02 17:24:20 -0500

    PurchaseDetail Dto's completed

--- src/purchase-detail/dto/create-purchase-detail.dto.ts
+++ src/purchase-detail/dto/create-purchase-detail.dto.ts
@@ -1,1 +1,48 @@
-export class CreatePurchaseDetailDto {}
+import { IsNotEmpty, IsNumber, Min, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreatePurchaseDetailDto {
+  @IsNumber()
+  @Min(1)
+  @ApiProperty()
+  quantity: number;
+
+  @IsNumber()
+  @Min(0)
+  @IsNotEmpty()
+  @ApiProperty()
+  unitPrice: number;
+
+  @IsNumber()
+  @Min(0)
+  @IsNotEmpty()
+  @ApiProperty()
+  subtotal: number;
+
+  @IsNumber()
+  @Min(1)
+  @IsNotEmpty()
+  @ApiProperty()
+  purchase: number;
+
+  @IsNumber()
+  @Min(1)
+  @IsNotEmpty()
+  @ApiProperty()
+  product: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly createdBy: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly updatedBy: number;
+
+  @IsNumber()
+  @IsOptional()
+  @ApiProperty()
+  readonly deletedBy: number | null;
+}
--- src/purchase-detail/dto/update-purchase-detail.dto.ts
+++ src/purchase-detail/dto/update-purchase-detail.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/swagger';
+import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreatePurchaseDetailDto } from './create-purchase-detail.dto';
 
-export class UpdatePurchaseDetailDto extends PartialType(CreatePurchaseDetailDto) {}
+export class UpdatePurchaseDetailDto extends PartialType(
+  OmitType(CreatePurchaseDetailDto, ['createdBy']),
+) {}

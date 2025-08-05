e4e45962dabaf6d29c6ed545de023c6dc8f24bef
Alex Coronell <alexcoronell@gmail.com>
2025-08-05 17:24:12 -0500

    shipment controller completed

--- src/shipment/shipment.controller.ts
+++ src/shipment/shipment.controller.ts
@@ -1,15 +1,41 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+
+/* Interface */
+import { IBaseController } from '@commons/interfaces/i-base-controller';
+
+/* Services */
 import { ShipmentService } from './shipment.service';
+
+/* Entities */
+import { Shipment } from './entities/shipment.entity';
+
+/* DTO's */
 import { CreateShipmentDto } from './dto/create-shipment.dto';
 import { UpdateShipmentDto } from './dto/update-shipment.dto';
 
 @Controller('shipment')
-export class ShipmentController {
+export class ShipmentController
+  implements IBaseController<Shipment, CreateShipmentDto, UpdateShipmentDto>
+{
   constructor(private readonly shipmentService: ShipmentService) {}
 
-  @Post()
-  create(@Body() createShipmentDto: CreateShipmentDto) {
-    return this.shipmentService.create(createShipmentDto);
+  @Get('count-all')
+  countAll() {
+    return this.shipmentService.countAll();
+  }
+
+  @Get('count')
+  count() {
+    return this.shipmentService.count();
   }
 
   @Get()
@@ -17,18 +43,41 @@ export class ShipmentController {
     return this.shipmentService.findAll();
   }
 
+  @Get('shipping-company/:id')
+  findAllByShippingCompanyId(@Param('id', ParseIntPipe) id: number) {
+    return this.shipmentService.findAllByShippingCompanyId(+id);
+  }
+
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: number) {
     return this.shipmentService.findOne(+id);
   }
 
+  @Get('tracking-number/:tracking')
+  findOneByTrackingNumber(@Param('tracking') tracking: string) {
+    return this.shipmentService.findOneByTrackingNumber(tracking);
+  }
+
+  @Get('sale/:id')
+  findBySaleId(@Param('id', ParseIntPipe) id: number) {
+    return this.shipmentService.findOneBySaleId(+id);
+  }
+
+  @Post()
+  create(@Body() payload: CreateShipmentDto) {
+    return this.shipmentService.create(payload);
+  }
+
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateShipmentDto: UpdateShipmentDto) {
-    return this.shipmentService.update(+id, updateShipmentDto);
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() changes: UpdateShipmentDto,
+  ) {
+    return this.shipmentService.update(+id, changes);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseIntPipe) id: number) {
     return this.shipmentService.remove(+id);
   }
 }

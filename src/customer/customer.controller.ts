5bea642875d4392217e6668f501f3e4134887d87
Alex Coronell <alexcoronell@gmail.com>
2025-08-19 22:41:21 -0500

    Customer controller completed

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,15 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
+/* Interfaces */
+import { IBaseController } from '@commons/interfaces/i-base-controller';
+
+/* Services */
 import { CustomerService } from './customer.service';
+
+/* Entities */
+import { Customer } from './entities/customer.entity';
+
+/* DTO's */
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
+import { UpdateCustomerPasswordDto } from './dto/update-customer-password';
 
+@ApiTags('Customers')
 @Controller('customer')
-export class CustomerController {
+export class CustomerController
+  implements IBaseController<Customer, CreateCustomerDto, UpdateCustomerDto>
+{
   constructor(private readonly customerService: CustomerService) {}
 
-  @Post()
-  create(@Body() createCustomerDto: CreateCustomerDto) {
-    return this.customerService.create(createCustomerDto);
+  @Get('count-all')
+  countAll() {
+    return this.customerService.countAll();
+  }
+
+  @Get('count')
+  count() {
+    return this.customerService.count();
   }
 
   @Get()
@@ -17,18 +46,44 @@ export class CustomerController {
     return this.customerService.findAll();
   }
 
+  @Get('actives')
+  findAllActives() {
+    return this.customerService.findAllActives();
+  }
+
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: Customer['id']) {
     return this.customerService.findOne(+id);
   }
 
+  @Get('email/:email')
+  findOneByEmail(@Param('email') email: string) {
+    return this.customerService.findOneByEmail(email);
+  }
+
+  @Post()
+  create(@Body() payload: CreateCustomerDto) {
+    return this.customerService.create(payload);
+  }
+
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCustomerDto: UpdateCustomerDto) {
-    return this.customerService.update(+id, updateCustomerDto);
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() changes: UpdateCustomerDto,
+  ) {
+    return this.customerService.update(id, changes);
+  }
+
+  @Patch('email/:id')
+  updatePassword(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() changes: UpdateCustomerPasswordDto,
+  ) {
+    return this.customerService.updatePassword(id, changes);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.customerService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.customerService.remove(id);
   }
 }

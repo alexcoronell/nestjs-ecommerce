3dd45e3b22b034b9c65c230088d3395893d25dec
Alex Coronell <alexcoronell@gmail.com>
2025-08-29 17:14:07 -0500

    user test:e2e completed with apikey guard

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,17 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/require-await */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
-import { UserModule } from '@user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-/* App Testing Module */
-//import { testModule } from './test.module';
+import { AppModule } from '../src/app.module';
+import { UserModule } from '@user/user.module';
 
 /* Seed */
 import { upSeed, downSeed } from './utils/seed';
@@ -22,6 +20,8 @@ import { dataSource } from './utils/seed';
 /* Seed */
 import { seedNewUser, seedUser, seedUsers } from './utils/user.seed';
 import { UpdateUserDto } from '@user/dto/update-user.dto';
+
+const API_KEY = process.env.API_KEY || 'api-e2e-key';
 
 describe('UserControler (e2e)', () => {
   let app: INestApplication<App>;
@@ -44,6 +44,7 @@ describe('UserControler (e2e)', () => {
             ...dataSource.options,
           }),
         }),
+        AppModule,
         UserModule,
       ],
     }).compile();
@@ -60,25 +61,63 @@ describe('UserControler (e2e)', () => {
   describe('GET User', () => {
     it('/count-all', async () => {
       await repo.save(seedUsers);
-      const data: any = await request(app.getHttpServer()).get(
-        '/user/count-all',
-      );
+      const data: any = await request(app.getHttpServer())
+        .get('/user/count-all')
+        .set('Authorization', API_KEY);
       const { statusCode, total } = data.body;
       expect(statusCode).toBe(200);
       expect(total).toEqual(seedUsers.length);
+    });
+
+    it('/count-all should return 401 if api key is missing', async () => {
+      const data: any = await request(app.getHttpServer()).get(
+        '/user/count-all',
+      );
+      const { body, statusCode } = data;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
+    });
+
+    it('/count-all should return 401 if api key is invalid', async () => {
+      const data: any = await request(app.getHttpServer())
+        .get('/user/count-all')
+        .set('Authorization', 'invalid-api-key');
+      const { body, statusCode } = data;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
     });
 
     it('/count', async () => {
       await repo.save(seedUsers);
-      const data = await request(app.getHttpServer()).get('/user/count');
+      const data = await request(app.getHttpServer())
+        .get('/user/count')
+        .set('Authorization', API_KEY);
       const { statusCode, total } = data.body;
       expect(statusCode).toBe(200);
       expect(total).toEqual(seedUsers.length);
     });
 
+    it('/count should return 401 if api key is missing', async () => {
+      const data: any = await request(app.getHttpServer()).get('/user/count');
+      const { body, statusCode } = data;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
+    });
+
+    it('/count should return 401 if api key is invalid', async () => {
+      const data: any = await request(app.getHttpServer())
+        .get('/user/count')
+        .set('Authorization', 'invalid-api-key');
+      const { body, statusCode } = data;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
+    });
+
     it('/', async () => {
       await repo.save(seedUsers);
-      const res = await request(app.getHttpServer()).get('/user');
+      const res = await request(app.getHttpServer())
+        .get('/user')
+        .set('Authorization', API_KEY);
       const { statusCode, data } = res.body;
       expect(statusCode).toBe(200);
       expect(data.length).toEqual(seedUsers.length);
@@ -94,11 +133,29 @@ describe('UserControler (e2e)', () => {
           }),
         );
       });
+    });
+
+    it('/ should return 401 if api key is missing', async () => {
+      const data: any = await request(app.getHttpServer()).get('/user');
+      const { body, statusCode } = data;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
+    });
+
+    it('/ should return 401 if api key is invalid', async () => {
+      const data: any = await request(app.getHttpServer())
+        .get('/user')
+        .set('Authorization', 'invalid-api-key');
+      const { body, statusCode } = data;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
     });
 
     it('/actives', async () => {
       await repo.save(seedUsers);
-      const res = await request(app.getHttpServer()).get('/user/actives');
+      const res = await request(app.getHttpServer())
+        .get('/user/actives')
+        .set('Authorization', API_KEY);
       const { statusCode, data } = res.body;
       expect(statusCode).toBe(200);
       expect(data.length).toEqual(seedUsers.length);
@@ -116,11 +173,27 @@ describe('UserControler (e2e)', () => {
       });
     });
 
+    it('/actives should return 401 if api key is missing', async () => {
+      const data: any = await request(app.getHttpServer()).get('/user/actives');
+      const { body, statusCode } = data;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
+    });
+
+    it('/actives should return 401 if api key is invalid', async () => {
+      const data: any = await request(app.getHttpServer())
+        .get('/user/actives')
+        .set('Authorization', 'invalid-api-key');
+      const { body, statusCode } = data;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
+    });
+
     it('/:id', async () => {
       await repo.save(seedUser);
-      const res = await request(app.getHttpServer()).get(
-        `/user/${seedUser.id}`,
-      );
+      const res = await request(app.getHttpServer())
+        .get(`/user/${seedUser.id}`)
+        .set('Authorization', API_KEY);
       const { statusCode, data } = res.body;
       expect(statusCode).toBe(200);
       expect(data.id).toEqual(seedUser.id);
@@ -130,11 +203,29 @@ describe('UserControler (e2e)', () => {
       expect(data.role).toEqual(seedUser.role);
     });
 
+    it('/:id should return 401 if api key is missing', async () => {
+      const id = seedUser.id;
+      const data: any = await request(app.getHttpServer()).get(`/user/${id}`);
+      const { body, statusCode } = data;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
+    });
+
+    it('/:id should return 401 if api key is invalid', async () => {
+      const id = seedUser.id;
+      const data: any = await request(app.getHttpServer())
+        .get(`/user/:${id}`)
+        .set('Authorization', 'invalid-api-key');
+      const { body, statusCode } = data;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
+    });
+
     it('/email/:email', async () => {
       await repo.save(seedUser);
-      const res = await request(app.getHttpServer()).get(
-        `/user/email/${seedUser.email}`,
-      );
+      const res = await request(app.getHttpServer())
+        .get(`/user/email/${seedUser.email}`)
+        .set('Authorization', API_KEY);
       const { statusCode, data } = res.body;
       expect(statusCode).toBe(200);
       expect(data.id).toEqual(seedUser.id);
@@ -142,6 +233,26 @@ describe('UserControler (e2e)', () => {
       expect(data.lastname).toEqual(seedUser.lastname);
       expect(data.email).toEqual(seedUser.email);
       expect(data.role).toEqual(seedUser.role);
+    });
+
+    it('/email/:email should return 401 if api key is missing', async () => {
+      const email = seedUser.email;
+      const data: any = await request(app.getHttpServer()).get(
+        `/user/email/${email}`,
+      );
+      const { body, statusCode } = data;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
+    });
+
+    it('/email/:email should return 401 if api key is invalid', async () => {
+      const email = seedUser.email;
+      const data: any = await request(app.getHttpServer())
+        .get(`/user/email/:${email}`)
+        .set('Authorization', 'invalid-api-key');
+      const { body, statusCode } = data;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
     });
   });
 
@@ -149,10 +260,29 @@ describe('UserControler (e2e)', () => {
     it('/ should create a user', async () => {
       const res = await request(app.getHttpServer())
         .post('/user')
+        .set('Authorization', API_KEY)
         .send(seedNewUser);
       const { statusCode, data } = res.body;
       expect(statusCode).toBe(201);
       expect(data.firstname).toEqual(seedNewUser.firstname);
+    });
+
+    it('/ should return 401 if api key is missing', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/user')
+        .send(seedNewUser);
+      const { body, statusCode } = res;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
+    });
+
+    it('/ should return 401 if api key is invalid', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/user')
+        .set('Authorization', 'invalid-api-key');
+      const { body, statusCode } = res;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
     });
   });
 
@@ -166,11 +296,43 @@ describe('UserControler (e2e)', () => {
       };
       const res = await request(app.getHttpServer())
         .patch(`/user/${id}`)
+        .set('Authorization', API_KEY)
         .send(updatedData);
       const { statusCode, data } = res.body;
       expect(statusCode).toBe(200);
       expect(data.firstname).toBe(updatedData.firstname);
       expect(data.lastname).toBe(updatedData.lastname);
+    });
+
+    it('/ should return 401 if api key is missing', async () => {
+      await repo.save(seedUsers);
+      const id = seedUsers[0].id;
+      const updatedData: UpdateUserDto = {
+        firstname: 'Updated name',
+        lastname: 'Updated lastname',
+      };
+      const res = await request(app.getHttpServer())
+        .patch(`/user/${id}`)
+        .send(updatedData);
+      const { body, statusCode } = res;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
+    });
+
+    it('/ should return 401 if api key is invalid', async () => {
+      await repo.save(seedUsers);
+      const id = seedUsers[0].id;
+      const updatedData: UpdateUserDto = {
+        firstname: 'Updated name',
+        lastname: 'Updated lastname',
+      };
+      const res = await request(app.getHttpServer())
+        .patch(`/user/${id}`)
+        .set('Authorization', 'invalid-api-key')
+        .send(updatedData);
+      const { body, statusCode } = res;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
     });
   });
 
@@ -178,13 +340,35 @@ describe('UserControler (e2e)', () => {
     it('/:id should delete a user', async () => {
       await repo.save(seedUsers);
       const id = seedUsers[0].id;
-      const res = await request(app.getHttpServer()).delete(`/user/${id}`);
+      const res = await request(app.getHttpServer())
+        .delete(`/user/${id}`)
+        .set('Authorization', API_KEY);
       const { statusCode } = res.body;
       const deletedInDB = await repo.findOne({
         where: { id, isDeleted: false },
       });
       expect(statusCode).toBe(204);
       expect(deletedInDB).toBeNull();
+    });
+
+    it('/ should return 401 if api key is missing', async () => {
+      await repo.save(seedUsers);
+      const id = seedUsers[0].id;
+      const res = await request(app.getHttpServer()).delete(`/user/${id}`);
+      const { body, statusCode } = res;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
+    });
+
+    it('/ should return 401 if api key is invalid', async () => {
+      await repo.save(seedUsers);
+      const id = seedUsers[0].id;
+      const res = await request(app.getHttpServer())
+        .delete(`/user/${id}`)
+        .set('Authorization', 'invalid-api-key');
+      const { body, statusCode } = res;
+      expect(statusCode).toBe(401);
+      expect(body).toHaveProperty('message', 'Invalid API key');
     });
   });
 

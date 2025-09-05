7b3e32ec894d267122b6d8fae9c89807a983ae15
Alex Coronell <alexcoronell@gmail.com>
2025-09-05 12:47:02 -0500

    Auth service test fixed

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -139,12 +139,12 @@ describe('AuthService', () => {
       it('should throw an error if refresh token secret is not defined', async () => {
         service.jwtRefreshTokenSecret = null;
         await expect(
-          service['generateRefreshToken']({ user: 1 }),
+          service['generateRefreshToken']({ user: 1, isAdmin: false }),
         ).rejects.toThrow('JWT refresh token secret is not defined');
       });
 
       it('should generate a refresh token', async () => {
-        const payload = { user: 1 };
+        const payload = { user: 1, isAdmin: false };
         const result = await service['generateRefreshToken'](payload);
         expect(result).toBe('mockedRefreshToken');
       });

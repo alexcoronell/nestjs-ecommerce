da6a94522024c6d398248579f73473c45945f69c
Alex Coronell <alexcoronell@gmail.com>
2025-09-10 15:55:20 -0500

    customer seed created and completed

---
+++ test/utils/customer.seed.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import * as bcrypt from 'bcrypt';
+
+/* Entity */
+import { Customer } from '@customer/entities/customer.entity';
+
+/* DTO's */
+import { CreateCustomerDto } from '@customer/dto/create-customer.dto';
+
+/* Faker */
+import {
+  createCustomer,
+  generateCustomer,
+  generateManyCustomers,
+} from '@faker/customer.faker';
+
+export const customerPasword = 'customer123';
+
+export const seedNewCustomer = async (): Promise<CreateCustomerDto> => ({
+  firstname: 'Alice',
+  lastname: 'Smith',
+  email: 'alice.smith@example.com',
+  password: await bcrypt.hash(customerPasword, 10),
+  phoneNumber: '123-456-7890',
+  department: 'Sales',
+  city: 'New York',
+  address: '123 Main St',
+  neighborhood: 'Downtown',
+});
+
+export const seedNewCustomerDynamic = createCustomer();
+
+export const seedCustomer: Customer = generateCustomer();
+
+export const seedCustomers: Customer[] = generateManyCustomers(10);
--- test/utils/user.seed.ts
+++ test/utils/user.seed.ts
@@ -15,11 +15,13 @@ import { UserRoleEnum } from '@commons/enums/user-role.enum';
 /* Faker */
 import { createUser, generateUser, generateManyUsers } from '@faker/user.faker';
 
+export const adminPassword = 'john123';
+
 export const seedNewAdminUser = async (): Promise<CreateUserDto> => ({
   firstname: 'John',
   lastname: 'Doe',
   email: 'johndoe@email.com',
-  password: await bcrypt.hash('john123', 10),
+  password: await bcrypt.hash(adminPassword, 10),
   phoneNumber: '555-55-55',
   role: UserRoleEnum.ADMIN,
   createdBy: 1,
@@ -27,21 +29,19 @@ export const seedNewAdminUser = async (): Promise<CreateUserDto> => ({
   deletedBy: null,
 });
 
-export const adminPassword = 'john123';
+export const sellerPassword = 'jane123';
 
 export const seedNewSellerUser = async (): Promise<CreateUserDto> => ({
   firstname: 'Jane',
   lastname: 'Doe',
   email: 'janedoe@email.com',
-  password: await bcrypt.hash('jane123', 10),
+  password: await bcrypt.hash(sellerPassword, 10),
   phoneNumber: '555-55-55',
   role: UserRoleEnum.SELLER,
   createdBy: 1,
   updatedBy: null,
   deletedBy: null,
 });
-
-export const sellerPassword = 'jane123';
 
 export const seedNewUser = createUser();
 
